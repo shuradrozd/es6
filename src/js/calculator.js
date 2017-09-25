cd8e068bc5441f2144b4f769fe1aeac303c061dc
AlexDrozd <shuradrozd@mail.ru>
2017-09-25 15:31:50 +0300

    Initial commit

---
+++ src/js/calculator.js
@@ -0,0 +1,12 @@
+const name = "alex";
+const calculator = {
+    sum (a,b) {
+        return a + b;
+    },
+    dif (a,b) {
+        return a - b;
+    }
+}
+
+export default calculator;
+export {name};
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import calculator, {name} from "./calculator"
+
+const a = 5;
+const b = 6;
+
+const sum = calculator.sum(a,b);
+console.log("test");
